4f33611bfc344ec6c2ecb7f9f79aa6dd1b25ce67
Tobias Lindberg (Dell) <tobias.lindberg@sodertalje.ntig.se>
2018-01-15 13:08:48 +0100

    Skapat Listener-metoden.

--- TobiasChattGitHub/TobiasChattGitHub/Program.cs
+++ TobiasChattGitHub/TobiasChattGitHub/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +13,35 @@ namespace TobiasChattGitHub
 
     class Program
     {
+        private const int ListenPort = 11000;
+
         static void Main(string[] args)
         {
+
+        }
+
+        static void Listener()
+        {
+            UdpClient listener = new UdpClient(ListenPort);
+
+            try
+            {
+                while (true)
+                {
+                    IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, ListenPort);
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    Console.WriteLine("Recieved broadcast from {0} : {1}\n",
+                        groupEP.ToString(), Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
         }
     }
 }
